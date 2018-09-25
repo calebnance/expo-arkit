96cc23e24fa13de46d1aab599efab2ac42110cf2
Caleb Nance <calebnance@gmail.com>
2018-09-24 23:58:08 -0400

    floating blue block
    shoutout EvanBacon: https://blog.expo.io/arkit-in-react-native-tutorial-the-basics-9f839539f0b9

--- App.js
+++ App.js
@@ -1,21 +1,64 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { AR } from 'expo';
+import ExpoTHREE, { AR as ThreeAR, THREE } from 'expo-three';
+import { View as GraphicsView } from 'expo-graphics';
 
-export default class App extends Component {
+export default class App extends React.Component {
+  componentDidMount() {
+    THREE.suppressExpoWarnings();
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Expo ARKit</Text>
-      </View>
+      <GraphicsView
+        style={{ flex: 1 }}
+        onContextCreate={this.onContextCreate}
+        onRender={this.onRender}
+        onResize={this.onResize}
+        isArEnabled
+        isArRunningStateEnabled
+        isArCameraStateEnabled
+        arTrackingConfiguration={AR.TrackingConfigurations.World}
+      />
     );
   }
-}
 
-let styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    backgroundColor: '#c0c5c1',
-    flex: 1,
-    justifyContent: 'center'
-  }
-});
+  onContextCreate = async ({ gl, scale: pixelRatio, width, height }) => {
+    AR.setPlaneDetection(AR.PlaneDetectionTypes.Horizontal);
+
+    this.renderer = new ExpoTHREE.Renderer({
+      gl,
+      pixelRatio,
+      width,
+      height
+    });
+
+    this.scene = new THREE.Scene();
+    this.scene.background = new ThreeAR.BackgroundTexture(this.renderer);
+    this.camera = new ThreeAR.Camera(width, height, 0.01, 1000);
+    const geometry = new THREE.BoxGeometry(0.1, 0.1, 0.1);
+    const material = new THREE.MeshPhongMaterial({
+      color: 0x096fec
+    });
+
+    this.cube = new THREE.Mesh(geometry, material);
+    this.cube.position.z = -0.4;
+    this.scene.add(this.cube);
+    this.scene.add(new THREE.AmbientLight(0xffffff));
+  };
+
+  onResize = ({ x, y, scale, width, height }) => {
+    if (!this.renderer) {
+      return;
+    }
+
+    this.camera.aspect = width / height;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setPixelRatio(scale);
+    this.renderer.setSize(width, height);
+  };
+
+  onRender = () => {
+    this.renderer.render(this.scene, this.camera);
+  };
+}
--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 ## install
 `yarn install`
 
+## project notes
+
 ### linting: prettier and airbnb config
 - make sure you have [prettier package](https://atom.io/packages/prettier-atom) installed on your atom/vscode editor
 - then make sure to enable automatic format on save (packages → prettier → toggle format on save)
@@ -10,3 +12,4 @@
 
 ### helpful links
 [prettier/eslint](https://blog.echobind.com/integrating-prettier-eslint-airbnb-style-guide-in-vscode-47f07b5d7d6a)
+[arkit in react native tutorial: the basics](https://blog.expo.io/arkit-in-react-native-tutorial-the-basics-9f839539f0b9)
