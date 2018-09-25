cd0c63a5e5da0bd794b947d85bcb637021761a16
Caleb Nance <calebnance@gmail.com>
2018-09-25 00:19:41 -0400

    shadow/lighting added and permission support added
    shoutout EvanBacon: https://blog.expo.io/arkit-shadows-in-react-native-f881e01a99f8

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
 import React from 'react';
-import { AR } from 'expo';
+import { AR, Permissions } from 'expo';
 import ExpoTHREE, { AR as ThreeAR, THREE } from 'expo-three';
 import { View as GraphicsView } from 'expo-graphics';
 
 export default class App extends React.Component {
+  state = { permission: false };
+
   componentDidMount() {
     THREE.suppressExpoWarnings();
+    ThreeAR.suppressWarnings();
+
+    this.getPermission();
   }
 
+  getPermission = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+
+    this.setState({
+      permission: status === 'granted'
+    });
+  };
+
   render() {
+    if (!this.state.permission) {
+      return null;
+    }
+
     return (
       <GraphicsView
         style={{ flex: 1 }}
         onContextCreate={this.onContextCreate}
         onRender={this.onRender}
         onResize={this.onResize}
+        isShadowsEnabled
         isArEnabled
         isArRunningStateEnabled
         isArCameraStateEnabled
@@ -33,19 +51,61 @@ export default class App extends React.Component {
       height
     });
 
+    this.renderer.gammaInput = this.renderer.gammaOutput = true;
+    this.renderer.shadowMap.enabled = true;
+
     this.scene = new THREE.Scene();
     this.scene.background = new ThreeAR.BackgroundTexture(this.renderer);
     this.camera = new ThreeAR.Camera(width, height, 0.01, 1000);
+
+    this.magneticObject = new ThreeAR.MagneticObject();
+    this.scene.add(this.magneticObject);
+
     const geometry = new THREE.BoxGeometry(0.1, 0.1, 0.1);
-    const material = new THREE.MeshPhongMaterial({
-      color: 0x096fec
-    });
+    const material = new THREE.MeshPhongMaterial({ color: 0x096fec });
 
     this.cube = new THREE.Mesh(geometry, material);
-    this.cube.position.z = -0.4;
-    this.scene.add(this.cube);
-    this.scene.add(new THREE.AmbientLight(0xffffff));
+
+    this.cube.castShadow = true;
+    this.cube.position.y = 0.05;
+    this.magneticObject.add(this.cube);
+
+    this.shadowFloor = new ThreeAR.ShadowFloor({
+      width: 1,
+      height: 1,
+      opacity: 0.6
+    });
+
+    this.magneticObject.add(this.shadowFloor);
+
+    this.scene.add(new THREE.AmbientLight(0x404040));
+
+    this.shadowLight = this.getShadowLight();
+    this.scene.add(this.shadowLight);
+    this.scene.add(this.shadowLight.target);
   };
+
+  getShadowLight = () => {
+    let light = new THREE.DirectionalLight(0xffffff, 0.6);
+
+    light.castShadow = true;
+
+    const shadowSize = 0;
+    light.shadow.camera.left = -shadowSize;
+    light.shadow.camera.right = shadowSize;
+    light.shadow.camera.top = shadowSize;
+    light.shadow.camera.bottom = -shadowSize;
+    light.shadow.camera.near = 0.001;
+    light.shadow.camera.far = 100;
+    light.shadow.camera.updateProjectionMatrix();
+
+    light.shadow.mapSize.width = 512 * 2;
+    light.shadow.mapSize.height = light.shadow.mapSize.width;
+
+    return light;
+  };
+
+  screenCenter = new THREE.Vector2(0.5, 0.5);
 
   onResize = ({ x, y, scale, width, height }) => {
     if (!this.renderer) {
@@ -59,6 +119,14 @@ export default class App extends React.Component {
   };
 
   onRender = () => {
+    this.magneticObject.update(this.camera, this.screenCenter);
+
+    this.shadowLight.target.position.copy(this.magneticObject.position);
+    this.shadowLight.position.copy(this.shadowLight.target.position);
+    this.shadowLight.position.x += 0.1;
+    this.shadowLight.position.y += 1;
+    this.shadowLight.position.z += 0.1;
+
     this.renderer.render(this.scene, this.camera);
   };
 }
--- README.md
+++ README.md
@@ -11,5 +11,6 @@
 - be aware of the `.prettierignore` file
 
 ### helpful links
-- [prettier/eslint](https://blog.echobind.com/integrating-prettier-eslint-airbnb-style-guide-in-vscode-47f07b5d7d6a)
+- [setup prettier/eslint within project](https://blog.echobind.com/integrating-prettier-eslint-airbnb-style-guide-in-vscode-47f07b5d7d6a)
 - [arkit in react native tutorial: the basics](https://blog.expo.io/arkit-in-react-native-tutorial-the-basics-9f839539f0b9)
+- [arkit shadows in react native](https://blog.expo.io/arkit-shadows-in-react-native-f881e01a99f8)
